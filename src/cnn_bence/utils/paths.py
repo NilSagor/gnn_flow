ec71c578af3f2fd29c4d3c08e0a1a7e3d0c07ac7
NilSagor <masum1201@gmail.com>
2025-10-22 16:51:23 +0800

    paths utility added

---
+++ src/cnn_bence/utils/paths.py
@@ -0,0 +1,10 @@
+
+from pathlib import Path
+
+def get_project_root():
+    current_file = Path(__file__)
+    for parent in current_file.parents:
+        if (parent/"pyproject.toml").exists():
+            return parent
+    raise FileNotFoundError("Could not find root file pyproject.toml") 
+        
